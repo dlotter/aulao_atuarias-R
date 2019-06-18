3c9b5e599adf3975b0789efd24f0e49ebca4eb32
dlotter <diegolottermann@usp.br>
2019-06-18 00:16:17 -0300

    ex4

---
+++ p2/ex4.R
@@ -0,0 +1,33 @@
+#fórmula geral
+
+f<-function(x){  
+}
+bissec<-function(a,b,nmax,TOL,f){
+  k<-1
+  while(k<nmax){
+    
+    if( f((a+b)/2) == 0 || abs(f((a+b)/2)) < TOL){
+      print("encontrado a raiz")
+      print((a+b)/2)
+      #abaixo virá o número de interações
+      print("número de interações")
+      print(k)
+      break
+    }else{
+      
+      pm<- (a+b)/2
+      if( sign(f(a)) == sign(f(pm) )){
+        a<-pm
+        print("não foi encontrada a raíz")
+        print(a)
+        
+      }else{
+        b<-pm 
+        print("não foi encontrada a raíz")
+        print(b)
+      }
+    }
+    k<-k+1
+  }
+  
+}
