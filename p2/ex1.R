7a7e3e2ba02fd965667897faa2a374e564598d69
Diego Lottermann <diegolottermann@usp.br>
2019-06-19 19:51:19 -0300

    Update ex1.R

--- p2/ex1.R
+++ p2/ex1.R
@@ -1,8 +1,12 @@
+# Exemplificando com 2 vetores:
 x <- c(1,2,3,4)
 y <- c(3,4,2,3)
+
+# Criando um vetor numerico vazio 
 z <- vector('numeric')
 
-for (c in 1:4) {
+# Algorítmo para os valores de z
+for (c in 1:len(x) {
   if (x[c] > y[c]){
     z[c] = x[c] + y[c]
   }
