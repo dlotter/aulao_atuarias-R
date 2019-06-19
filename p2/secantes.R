b446a79b01b0e4b4c7fe753f7029413eab516959
dlotter <diegolottermann@usp.br>
2019-06-19 16:06:27 -0300

    secantes

---
+++ p2/secantes.R
@@ -0,0 +1,21 @@
+f <- function(){ }
+
+fsec <- function(xAnt0, xAnt1, nmax, TOL){
+  k <- 1
+  
+  while(k <= nmax){
+    xAtual = xAnt1 - (xAnt0 - xAnt1)*f(xAnt1)/(f(xAnt0) - f(xAnt1))
+  
+    Er = abs(xAtual-XAnt1/xAtual)
+    
+    if(Er < TOL){
+      print('Valor encontrado')
+      print(xAtual)
+      print('Número de interações')
+      print(k)
+    }else{
+      xAnt0 = xAnt1
+      xAnt1 = xAtual
+    }
+  }
+}
